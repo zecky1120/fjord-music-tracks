dcec257eb1af97548f8df9f51627e85ffc75d027
zecky1120 <takahide1120@gmail.com>
2026-04-30 21:20:07 +0900

    ユーザーモデルを作成

---
+++ db/migrate/20260430121235_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :profile
+      t.string :avator_url
+      t.string :uid
+      t.string :provider
+
+      t.timestamps
+    end
+  end
+end
