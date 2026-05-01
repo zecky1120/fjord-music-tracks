67d54c48d37222fc469004acf996855b4d6275c9
zecky1120 <takahide1120@gmail.com>
2026-05-01 21:05:30 +0900

    credentialの追加とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions, only: %i[create destroy]
 end
