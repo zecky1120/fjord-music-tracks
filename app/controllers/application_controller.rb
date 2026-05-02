5a62cec24401299e4e3277069e9542e5ae1c12b7
zecky1120 <takahide1120@gmail.com>
2026-05-02 19:05:35 +0900

    applicationControllerにログイン関連のメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  include SessionsHelper
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if current_user
+
+    redirect_to root_path
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,3 @@
 class SessionsController < ApplicationController
+  skip_before_action :check_logged_in, only: :create
 end
