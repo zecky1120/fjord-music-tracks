1626492a1531615998effa54b2d9e9185aad55b2
zecky1120 <takahide1120@gmail.com>
2026-05-08 22:04:09 +0900

    profileカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_30_121235) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_125910) do
   create_table "users", force: :cascade do |t|
     t.string "avator_url"
     t.datetime "created_at", null: false
     t.string "name"
-    t.string "profile"
     t.string "provider"
     t.string "uid"
     t.datetime "updated_at", null: false
