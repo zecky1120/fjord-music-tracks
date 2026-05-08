1fad5e9fd4372beb667a271e3ed7082caf26de3d
zecky1120 <takahide1120@gmail.com>
2026-05-08 21:11:15 +0900

    scopeをidentifyに変更しログイン失敗した時の処理を実装

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :discord,
-  Rails.application.credentials.discord[:client_id],
-  Rails.application.credentials.discord[:client_secret],
-  scope: 'email'
+  provider :discord, Rails.application.credentials.discord[:client_id], Rails.application.credentials.discord[:client_secret], scope: 'identify'
+
+  OmniAuth.config.on_failure = proc { |_env| [ 302, { 'Location' => '/auth/failure', 'Content-Type' => 'text/html' }, [] ] }
 end
