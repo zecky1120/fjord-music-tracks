9a7eade272f3db198ceab265a986179bcf996466
zecky1120 <takahide1120@gmail.com>
2026-08-01 19:56:41 +0900

    Youtubeのサムネなどを表示させるためのAPIを設定

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'google/apis/youtube_v3'
+
 class Video < ApplicationRecord
   belongs_to :user
+
+  youtube = Google::Apis::YoutubeV3::YouTubeService.new
+  youtube.key = Rails.application.credentials.youtube[:api_key]
+
 end
