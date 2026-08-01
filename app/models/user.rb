b6ad0963389474733ecf80393103983477ce127a
zecky1120 <takahide1120@gmail.com>
2026-08-01 19:57:59 +0900

    userモデルにhas_manyモデルのvideosを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :videos
+
   def self.find_or_create_from_discord_auth(discord_auth)
     find_or_create_by(uid: discord_auth.uid) do |user|
       user.update(
