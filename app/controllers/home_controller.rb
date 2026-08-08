45c9d45d8200c926ee4e11ae88c70436cba0c74a
zecky1120 <takahide1120@gmail.com>
2026-08-08 20:01:56 +0900

    トップページに動画一覧を表示するよう実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
   skip_before_action :check_logged_in, only: :index
 
   def index
-    @users = User.all
+    @videos = Video.all.order(created_at: :desc)
   end
 end
