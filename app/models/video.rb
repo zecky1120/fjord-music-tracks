3024da8ae5e3cb522e9a0af14f09b1dec83a90e0
zecky1120 <takahide1120@gmail.com>
2026-08-08 19:50:42 +0900

    受け取った値をvideoオブジェクトとしてDB保存

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
-require 'google/apis/youtube_v3'
-
 class Video < ApplicationRecord
   belongs_to :user
 
-  youtube = Google::Apis::YoutubeV3::YouTubeService.new
-  youtube.key = Rails.application.credentials.youtube[:api_key]
+  validates :discord_channel_id, :discord_message_id, :title, :youtube_id, :user, presence: true
 
+  def self.create_video_info(discord_message)
+    user = User.find_by(uid: discord_message[:user_uid])
+
+    create!(
+      caption: discord_message[:caption],
+      discord_channel_id: discord_message[:discord_channel_id],
+      discord_message_id: discord_message[:discord_message_id],
+      youtube_id: discord_message[:youtube_id],
+      title: discord_message[:title],
+      user: user
+    )
+  end
 end
